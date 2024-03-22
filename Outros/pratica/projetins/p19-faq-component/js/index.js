855bd7f25973556079eb192e98b6118ec7d51bf7
Gabriel Bueno <106624338+gabriel-viniciusbueno@users.noreply.github.com>
2024-03-22 08:39:58 -0300

    Iniciando JS

---
+++ Outros/pratica/projetins/p19-faq-component/js/index.js
@@ -0,0 +1,13 @@
+const questionBtn = document.querySelectorAll('.question-btn')
+
+const minusBtn = document.querySelectorAll('.minus-btn')
+const plusBtn = document.querySelectorAll('.plus-btn')
+
+const answer = document.querySelectorAll('.question-answer p')
+
+questionBtn.forEach((btn) => {
+
+  btn.target.addEventListener('click', (e) => {
+
+  })
+})
