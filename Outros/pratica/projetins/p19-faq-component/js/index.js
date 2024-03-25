a31305c98ea6efe899bd14e3c678ce599a527506
Gabriel Bueno <106624338+gabriel-viniciusbueno@users.noreply.github.com>
2024-03-25 09:09:25 -0300

    Description content toggle

--- Outros/pratica/projetins/p19-faq-component/js/index.js
+++ Outros/pratica/projetins/p19-faq-component/js/index.js
@@ -1,12 +1,13 @@
 const questionBtn = document.querySelectorAll('.question-btn')
 
-const minusBtn = document.querySelectorAll('.minus-btn')
-const plusBtn = document.querySelectorAll('.plus-btn')
-
-const answer = document.querySelectorAll('.question-answer p')
 
 questionBtn.forEach((btn) => {
   btn.addEventListener('click', function (){
-    this.classList.toggle('hide')
+    // this.classList.toggle('hide')
+    const accordionContent = this.nextElementSibling
+    accordionContent.classList.toggle('hide')
+    const plusBtn = this.querySelector('.plus-btn')
+    const minusBtn = this.querySelector('.minus-btn')
+    console.log(accordionContent, plusBtn)
   })
 })
