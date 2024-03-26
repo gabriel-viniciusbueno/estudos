c11e148e0698fda0f7ed3ce6782d4dff3dc71bf2
Gabriel Bueno <106624338+gabriel-viniciusbueno@users.noreply.github.com>
2024-03-26 07:39:06 -0300

    projeto finalizado

--- Outros/pratica/projetins/p19-faq-component/js/index.js
+++ Outros/pratica/projetins/p19-faq-component/js/index.js
@@ -5,9 +5,18 @@ questionBtn.forEach((btn) => {
   btn.addEventListener('click', function (){
     // this.classList.toggle('hide')
     const accordionContent = this.nextElementSibling
-    accordionContent.classList.toggle('hide')
     const plusBtn = this.querySelector('.plus-btn')
     const minusBtn = this.querySelector('.minus-btn')
-    console.log(accordionContent, plusBtn)
+
+
+    if(accordionContent.style.maxHeight) {
+      accordionContent.style.maxHeight = null
+      plusBtn.style.display = 'block'
+      minusBtn.style.display = 'none'
+    } else{
+      accordionContent.style.maxHeight = accordionContent.scrollHeight + 'px'
+      plusBtn.style.display = 'none'
+      minusBtn.style.display = 'block'
+    }
   })
 })
