6435517c608ed7884fc180c7b1007d43a757133d
Gabriel Bueno <106624338+gabriel-viniciusbueno@users.noreply.github.com>
2024-04-30 06:58:37 -0300

    share icons section

---
+++ Outros/pratica/projetos/p20-article-preview-component/js/index.js
@@ -0,0 +1,13 @@
+let shareSection = document.querySelector('.share-section')
+let shareBtn = document.querySelector('.share-icon')
+let socialIco = document.querySelectorAll('.social-icon')
+
+shareBtn.addEventListener('click', () => {
+  shareSection.classList.toggle('active')
+})
+
+socialIco.forEach((icon) => {
+  icon.addEventListener('click', () => {
+    icon.style.color = 'black'
+  })
+})
