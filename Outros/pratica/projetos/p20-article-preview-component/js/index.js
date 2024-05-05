6a4c6cc59e4125fb9f7795819f5c28c87209e284
Gabriel Bueno <106624338+gabriel-viniciusbueno@users.noreply.github.com>
2024-05-05 19:48:00 -0300

    Responsive

--- Outros/pratica/projetos/p20-article-preview-component/js/index.js
+++ Outros/pratica/projetos/p20-article-preview-component/js/index.js
@@ -1,7 +1,5 @@
 const shareOption = document.querySelector('.share-option')
-const shareButton = document.querySelector('.share-button')
-
-shareButton.addEventListener('click', function() {
-  // this.classList.toggle('active')
+document.querySelector('.share-button').addEventListener('click', function () {
+  this.classList.toggle('active')
   shareOption.classList.toggle('active')
 })
