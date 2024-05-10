74ff59d2dab4f011fe78d7a0e3a8bdaa348d471e
Gabriel Bueno <106624338+gabriel-viniciusbueno@users.noreply.github.com>
2024-05-10 10:16:39 -0300

    Game logic

--- Outros/Udemy/Jonas Schmedtmann/Aulas/project#1/script.js
+++ Outros/Udemy/Jonas Schmedtmann/Aulas/project#1/script.js
@@ -1,15 +1,37 @@
 'use strict';
 
 let secretNumber = Math.trunc(Math.random() * 20) + 1;
-document.querySelector('.number').textContent = number
+document.querySelector('.number').textContent = secretNumber
+
+let score = 20;
 
 document.querySelector('.check').addEventListener('click', () => {
   const guess = Number(document.querySelector('.guess').value)
   console.log(guess, typeof guess)
-})
+
 
 if (!guess) {
   document.querySelector('.message').textContent = 'No number!'
-} else if (geuss === secretNumber){
-
+} else if (guess === secretNumber){
+  document.querySelector('.message').textContent = 'Corret Number'
+} else if (guess > secretNumber) {
+  if (score > 1){
+    document.querySelector('.message').textContent = 'Too High'
+    score--
+    document.querySelector('.score').textContent = score
+  } else {
+    document.querySlector('.message').textContent = 'You lost the game!'
+    document.querySelector('.score').textContent = 0
+  }
+} else if (guess < secretNumber) {
+  if (score > 1){
+    document.querySelector('.message').textContent = 'Too low!'
+    score--
+    document.querySelector('.score').textContent = score
+  } else {
+    document.querySelector('.message').textContent = 'You lost the game!'
+    document.querySelector('.score').textContent = 0
+  }
 }
+
+})
