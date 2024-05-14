03697110acc6a228880539bc84568bcf814e4b8f
Gabriel Bueno <106624338+gabriel-viniciusbueno@users.noreply.github.com>
2024-05-14 09:03:52 -0300

    highscores system

--- Outros/Udemy/Jonas Schmedtmann/Aulas/project#1/script.js
+++ Outros/Udemy/Jonas Schmedtmann/Aulas/project#1/script.js
@@ -4,6 +4,7 @@ const resetBtn = document.querySelector('.again')
 let secretNumber = Math.trunc(Math.random() * 20) + 1;
 
 let score = 20;
+let highscore = 0
 document.querySelector('.check').addEventListener('click', () => {
   const guess = Number(document.querySelector('.guess').value)
   console.log(guess, typeof guess)
@@ -18,9 +19,15 @@ document.querySelector('.check').addEventListener('click', () => {
 } else if (guess === secretNumber){
   document.querySelector('.message').textContent = 'Corret Number!'
   document.querySelector('.number').textContent = secretNumber
+  
+  if(highscore < score){
+    highscore = score
+    document.querySelector('.highscore').textContent = highscore
+  }
+
+
 
   document.querySelector('body').style.backgroundColor = '#60b347'
-
   document.querySelector('.number').style.width = '30rem'
 
   // guess is too high
@@ -49,12 +56,13 @@ document.querySelector('.check').addEventListener('click', () => {
 })
 
 resetBtn.addEventListener('click', function (){
-  secretNumber = Math.trunc(Math.random() * 20) + 1
   score = 20
-  document.querySelector('.number').textContent = '?'
-  document.querySelector('.number').style.width = '15rem'
-  document.querySelector('body').style.backgroundColor = '#222'
+  secretNumber = Math.trunc(Math.random() * 20) + 1
+
   document.querySelector('.message').textContent = 'Start Guessing...'
   document.querySelector('.score').textContent = score
+  document.querySelector('.number').textContent = '?'
   document.querySelector('.guess').value = ''
+  document.querySelector('body').style.backgroundColor = '#222'
+  document.querySelector('.number').style.width = '15rem'
 })
