f9e978b10f3b5f2a6a5dd155545d9be300e43a29
Gabriel Bueno <106624338+gabriel-viniciusbueno@users.noreply.github.com>
2024-05-17 23:32:20 -0300

    modal logic

--- Outros/Udemy/Jonas Schmedtmann/Aulas/project#2/script.js
+++ Outros/Udemy/Jonas Schmedtmann/Aulas/project#2/script.js
@@ -3,11 +3,23 @@
 const modal = document.querySelector('.modal')
 const overlay = document.querySelector('.overlay')
 const btnCloseModal = document.querySelector('.close-modal')
-const btnsOpenModal = document.querySelectorAll('.open-modal')
+const btnsOpenModal = document.querySelectorAll('.show-modal')
+
+const openModal = function () {
+  modal.classList.remove('hidden')
+  overlay.classList.remove('hidden')
+}
+
+const closeModal = function () {
+  modal.classList.add('hidden')
+  overlay.classList.add('hidden')
+}
 
 for(let i = 0; i < btnsOpenModal.length; i++)
-  btnsOpenModal[i].addEventListener('click', function () {
-    console.log('Button clicked')
-    modal.classList.remove('hidden')
-  })
+  btnsOpenModal[i].addEventListener('click', openModal)
+
+
+
+btnCloseModal.addEventListener('click', closeModal)
+overlay.addEventListener('click', closeModal)
 
