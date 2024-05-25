6bfd0b6f9e705933fe7b8e7c778264e779dc39d2
Gabriel Bueno <106624338+gabriel-viniciusbueno@users.noreply.github.com>
2024-05-25 08:50:08 -0300

    finishing the game

--- Outros/Udemy/Jonas Schmedtmann/Aulas/project#3/script.js
+++ Outros/Udemy/Jonas Schmedtmann/Aulas/project#3/script.js
@@ -14,14 +14,26 @@ const btnRoll = document.querySelector('.btn--roll')
 const btnHold = document.querySelector('.btn--hold')
 
 //  Starting conditions
-score0El.textContent = 0
-score1El.textContent = 0
-diceEl.classList.add('hidden')
 
-const scores = [0, 0]
-let currentScore = 0
-let activePlayer = 0
-let playing = true
+let scores, currentScore, activePlayer, playing
+
+function init(){
+  scores = [0, 0]
+  currentScore = 0
+  activePlayer = 0
+  playing = true
+
+  score0El.textContent = 0
+  score1El.textContent = 0
+  current0El.textContent = 0
+  current1El.textContent = 0
+
+  diceEl.classList.add('hidden')
+  player0El.classList.remove('player--winner')
+  player1El.classList.remove('player--winner')
+  player0El.classList.add('player--active')
+  player1El.classList.remove('player--active')
+}
 
 function switchPlayer(){
   document.getElementById(`current--${activePlayer}`).textContent = 0
@@ -30,7 +42,7 @@ function switchPlayer(){
   player0El.classList.toggle('player--active')
   player1El.classList.toggle('player--active')
 }
-
+init()
 // Rolling dice functionality
 btnRoll.addEventListener('click', function() {
   if(playing){
@@ -59,7 +71,7 @@ btnHold.addEventListener('click', function (){
     scores[activePlayer] += currentScore
     document.getElementById(`score--${activePlayer}`).textContent = scores[activePlayer]
 
-    if(scores[activePlayer] >= 20){
+    if(scores[activePlayer] >= 100){
       playing = false
       diceEl.classList.add('hidden')
       
@@ -71,20 +83,4 @@ btnHold.addEventListener('click', function (){
   }
 })
 
-btnNew.addEventListener('click', function (){
-  score0El.textContent = 0
-  score1El.textContent = 0
-  current0El.textContent = 0
-  current1El.textContent = 0
-  player0El.classList.remove('player--winner')
-  player1El.classList.remove('player--winner')
-  player0El.classList.add('player--active')
-  player1El.classList.remove('player--active')
-
-
-  scores[0] = 0
-  scores[1] = 0
-  currentScore = 0
-  activePlayer = 0
-  playing = true
-})
+btnNew.addEventListener('click', init)
