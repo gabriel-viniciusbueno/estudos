317815aee4014df1dc9f807bfef91d0510f03527
Gabriel Bueno <106624338+gabriel-viniciusbueno@users.noreply.github.com>
2024-06-07 09:07:13 -0300

    hoisting and TDZ

--- Outros/Udemy/Jonas Schmedtmann/Aulas/scoping/js/script.js
+++ Outros/Udemy/Jonas Schmedtmann/Aulas/scoping/js/script.js
@@ -1,30 +1,63 @@
 'use strict'
 
-function calcAge(birthYear){
-  const age = 2037 - birthYear
+// function calcAge(birthYear){
+//   const age = 2037 - birthYear
 
-  function printAge(){
-    const output = `${firstName}, you are ${age}, born in ${birthYear}`
-    console.log(output);
+//   function printAge(){
+//     const output = `${firstName}, you are ${age}, born in ${birthYear}`
+//     console.log(output);
 
-    if(birthYear >= 1981 && birthYear <= 1996){
-      var millenial = true
-      const firstName = 'Steven'
-      const str = `Oh, and you're a millenial, ${firstName}`
-      console.log(str)
+//     if(birthYear >= 1981 && birthYear <= 1996){
+//       var millenial = true
+//       const firstName = 'Steven'
+//       const str = `Oh, and you're a millenial, ${firstName}`
+//       console.log(str)
       
-      function add(a, b){
-        return a + b
-      }
-    }
-    console.log(millenial)
-    add(2, 3)
+//       function add(a, b){
+//         return a + b
+//       }
+//     }
+//     console.log(millenial)
+//     add(2, 3)
 
-  }
-  printAge()
+//   }
+//   printAge()
 
-  return age
+//   return age
+// }
+
+// const firstName = 'Jonas'
+// calcAge(1991)
+
+// console.log(me)
+// console.log(job);
+// console.log(year);
+
+// var me = 'Jonas'
+// let job ='teacher'
+// const year = 1991
+
+
+// console.log(addDecl(2,3));
+// console.log(addExpr(2,3));
+// console.log(addArrow(2,3));
+
+// function addDecl(a,b){
+//   return a + b
+// }
+
+// const addExpr = function (a,b){
+//   return a + b
+// }
+
+// const addArrow = (a,b) => a + b
+
+console.log(numProducts)
+
+if(!numProducts) deleteShoppingCart()
+
+  var numProducts = 10
+
+function deleteShoppingCart() {
+  console.log('All products deleted!');
 }
-
-const firstName = 'Jonas'
-calcAge(1991)
