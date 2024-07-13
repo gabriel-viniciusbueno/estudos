184e06a197d733c599b459ca77055a8837a10e85
Gabriel Bueno <106624338+gabriel-viniciusbueno@users.noreply.github.com>
2024-07-12 23:06:06 -0300

    realocate

--- Outros/Udemy/Maximilian Schwarzmuller/aulas/a04/app.js
+++
@@ -1,5 +0,0 @@
-const listItemElements = document.getElementsByTagName('li') // live list
-
-for (const listItemEl of listItemElements) {
-  console.dir(listItemEl)
-}
---
+++ Outros/pratica/codewars/ex11/index.js
@@ -0,0 +1,10 @@
+// function solution(str, ending) {
+  // }
+  //   return str.split('').filter();
+  
+  
+  
+  function solution(str, ending){
+    return str.endsWith(ending);
+  }
+  console.log(solution("longword", "word"));
