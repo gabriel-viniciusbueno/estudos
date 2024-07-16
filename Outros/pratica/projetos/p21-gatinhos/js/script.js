6e93e70a3e9e5a7b84db932d4eaff8a896b63e13
Gabriel Bueno <106624338+gabriel-viniciusbueno@users.noreply.github.com>
2024-07-15 22:15:43 -0300

    rate cuties project

---
+++ Outros/pratica/projetos/p21-gatinhos/js/script.js
@@ -0,0 +1,8 @@
+const button = document.querySelectorAll('.button');
+
+// button.forEach((btn) => {
+//   btn.addEventListener('click', () => {
+//     console.log('fui clicado ' + btn.value)
+//   })
+// })
+
