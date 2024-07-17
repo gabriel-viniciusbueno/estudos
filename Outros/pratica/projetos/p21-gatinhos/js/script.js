9895e4b92f105284c37b9330573e417a0ca12f24
Gabriel Bueno <106624338+gabriel-viniciusbueno@users.noreply.github.com>
2024-07-16 21:59:01 -0300

    images & flex containers

--- Outros/pratica/projetos/p21-gatinhos/js/script.js
+++ Outros/pratica/projetos/p21-gatinhos/js/script.js
@@ -6,3 +6,5 @@ const button = document.querySelectorAll('.button');
 //   })
 // })
 
+// <!-- a ideia é usar template literals para mudar a foto exibida -->
+// armazenar o valor dos rates em um array e apenas retornar quando o quiz estiver encerrado. retirar o último valor do array caso o usuário queira voltar e refazer seus ratings
