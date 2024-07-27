cf7681a548a16ed11a7471352bada50e88b0c39f
Gabriel Bueno <106624338+gabriel-viniciusbueno@users.noreply.github.com>
2024-07-26 22:49:39 -0300

    next animal

--- Outros/pratica/projetos/p21-gatinhos/js/script.js
+++ Outros/pratica/projetos/p21-gatinhos/js/script.js
@@ -1,7 +1,9 @@
 // import module;
 const buttons = document.querySelectorAll('.btn-container .button');
-const arrow = document.querySelectorAll('.arrow-container a');
+const arrowPrev = document.getElementById('prev-arrow');
+const arrowNext = document.getElementById('next-arrow');
 
+let pictureId = 1;
 let rate
 let ratesArr = [];
 
@@ -23,26 +25,29 @@ buttons.forEach((btn) => btn.addEventListener('click', () => {
 })
 )
 
-arrow.forEach((a) => a.addEventListener('click', () => {
+arrowPrev.addEventListener('click', () => {
+  console.log('aoba')
+})
+
+arrowNext.addEventListener('click', () => {
+  pictureId++
+  const picture = document.querySelector('.img-container img');
+  picture.src = `./assets/animal${pictureId}.jpg`;
   console.log('clicado!')
 
   // mudar a foto
   // zerar os rate
   // adicionar o botão de voltar
-  
+  // impedir o usuário de avançar sem dar uma nota
+  //verificar se tem algum select nos rates, se tiver pode avançar
+
 })
-)
 
-//pegar os botões das notas
-//pegar os botões de  avançar
-// criar uma variavel igual a 0 que acompanha o numero da foto
+//fazer pictureId reduzir 1 quando voltarmos
+//Apagar o último elemento do array
+//voltar à foto anterior
+
 //criar um array para armazenar as notas. se avançar, a nota é armazenada no array. se voltar, a ultima nota do array é apagada.
-
-// button.forEach((btn) => {
-//   btn.addEventListener('click', () => {
-//     console.log('fui clicado ' + btn.value)
-//   })
-// })
 
 // <!-- a ideia é usar template literals para mudar a foto exibida -->
 // armazenar o valor dos rates em um array e apenas retornar quando o quiz estiver encerrado. retirar o último valor do array caso o usuário queira voltar e refazer seus ratings
