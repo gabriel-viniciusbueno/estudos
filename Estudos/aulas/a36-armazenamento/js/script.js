4e721be4056cfc90ce40bf61133bef5ad1f17909
Gabriel Bueno <106624338+gabriel-viniciusbueno@users.noreply.github.com>
2024-08-13 21:04:03 -0300

    cookies

--- Estudos/aulas/a36-armazenamento/js/script.js
+++ Estudos/aulas/a36-armazenamento/js/script.js
@@ -19,3 +19,21 @@ document.getElementById('readLocal').addEventListener('click', () => {
   const info = localStorage.getItem('info')
   alert(`A informação armazenada em local storage é ${info}`)
 })
+
+document.getElementById('cookieBtn').addEventListener('click', () => {
+  const input = document.getElementById('cookie');
+  const cookie = `info=${input.value};`; //nome e valor do cookie
+  const expiration = `expires=${new Date(2024, 9, 9)};`; // expiração do cookie
+  const path = `path=/;`; // path onde o cookie poderá ser acessado
+  document.cookie = cookie + expiration + path; // criação do cookie
+  input.value = ''
+})
+
+document.getElementById('cookie2Btn').addEventListener('click', () => {
+  const input = document.getElementById('cookie2');
+  const cookie = `text=${input.value};`;
+  const expiration = `expires=${new Date(2024, 8, 7)};`;
+  const path = `path=/;`
+  document.cookie = cookie + expiration + path;
+
+})
