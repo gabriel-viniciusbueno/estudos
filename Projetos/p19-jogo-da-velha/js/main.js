c863d2fdc8e9f1b5f65e13879a9b0f2ba8c44eb6
Gabriel Bueno <106624338+gabriel-viniciusbueno@users.noreply.github.com>
2024-08-22 22:58:10 -0300

    selectcell function

--- Projetos/p19-jogo-da-velha/js/main.js
+++ Projetos/p19-jogo-da-velha/js/main.js
@@ -13,13 +13,20 @@ document.querySelectorAll('.cells').forEach((cell) => {
   cell.addEventListener('click', () => {
     playerOne = playerNames[0]
     playerTwo = playerNames[1]
+
+    selectCell(cell)
     switchPlayer(playerOne, playerTwo)
-    // updatePlayerTurnHeader(currentPlayer)
     // getting cell data index
     // console.log(cell.getAttribute('data-index-cell'))
      
   })
 })
+
+function selectCell(cell){
+  // criar elemento html e indexar ao cell
+  const selectedCell = document.element('i class="fa-solid fa-xmark"')
+  cell.appendChild(selectedCell)
+}
 
 // gametable events (selected cells, invalid cells)
   // get cellIndex with getattribute, and fill gamestate array
