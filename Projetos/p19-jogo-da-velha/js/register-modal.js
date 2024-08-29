6b07bbec54391ad3d5c46613fa43e3ac16d175bd
Gabriel Bueno <106624338+gabriel-viniciusbueno@users.noreply.github.com>
2024-08-28 21:22:01 -0300

    little name verification

--- Projetos/p19-jogo-da-velha/js/register-modal.js
+++ Projetos/p19-jogo-da-velha/js/register-modal.js
@@ -18,13 +18,17 @@ function displayGameBoard(){
 export const playerNames = []
 
 export function startGame(){
-  const player1Input = document.getElementById('name1')
-  const player2Input = document.getElementById('name2')
+  const player1Input = document.getElementById('name1').value
+  const player2Input = document.getElementById('name2').value
 
+  if(player1Input == '' || player2Input == '' ){
+    alert('Please, name the players')
+    return
+  }
   const playerTurnHeader = document.querySelector('.game-container h2 span')
   playerTurnHeader.innerHTML = player1Input.value
 
-  playerNames.push(player1Input.value, player2Input.value);
+  playerNames.push(player1Input, player2Input);
 
   let playerOne = playerNames[0]
   let playerTwo = playerNames[1]
