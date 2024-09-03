57bd806affffff3132f9f1d61a05b74ed23dab8b
Gabriel Bueno <106624338+gabriel-viniciusbueno@users.noreply.github.com>
2024-09-03 06:35:42 -0300

    spread operator

--- Estudos/aulas/a38-spread/script.js
+++ Estudos/aulas/a38-spread/script.js
@@ -2,3 +2,19 @@ const towns = ['Prontera', 'Izlude', 'Payon', 'Alberta']
 
 console.log(...towns)
 console.log(...towns[1])
+
+const townsCopy = towns
+
+townsCopy.pop()
+townsCopy.pop()
+townsCopy.push('Juno')
+
+console.log({ towns, townsCopy})
+
+const townsClone = [...towns]
+
+townsClone.pop()
+townsClone.pop()
+townsClone.push('Pallet City')
+
+console.log(townsClone)
