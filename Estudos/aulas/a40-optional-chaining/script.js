e03460ab31c897303622ca511cbf85d1c26d02c7
Gabriel Bueno <106624338+gabriel-viniciusbueno@users.noreply.github.com>
2024-09-12 06:45:11 -0300

    nullish coalescing

---
+++ Estudos/aulas/a40-optional-chaining/script.js
@@ -0,0 +1,23 @@
+const user = {
+  name: "John Doe",
+  email: "doejohn@email.com",
+  friends: [{
+    name: "Mary",
+    address: {
+      street: "Some Street",
+      number: 89
+    }
+  }],
+  age: 42,
+  phone: {
+    countryCode: "+55",
+    ddd: "22",
+    number: "998765432"
+  }
+}
+
+console.log(user.friends[0].phone.ddd)
+// erro cannot read properties
+
+console.log(user.friends[0].phone?.ddd)
+// mostrará undefined
---
+++ Estudos/aulas/a41-nullish-coalescing/script.js
@@ -0,0 +1,5 @@
+const a = 0
+const b = null
+const c = 'Teste'
+
+console.log(a ?? b ?? c)
